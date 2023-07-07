c8086828832ad24ef5e15a73180df99787b09d43
Matias Pellegrino <matiaspellegrino@outlook.com>
2023-07-07 07:50:28 -0300

    Realizo cambios en template

--- template/js/main.js
+++ template/js/main.js
@@ -111,13 +111,7 @@
     });
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+  
 
 })(jQuery);
 
